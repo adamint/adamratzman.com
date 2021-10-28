89f1f90be91f3503f7a36aab2d2ac870ed8685da
Adam Ratzman <adam@adamratzman.com>
2021-10-28 16:25:01 -0400

    add audio preview

--- src/components/projects/spotify/views/SpotifyTrack.tsx
+++ src/components/projects/spotify/views/SpotifyTrack.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, FlexProps, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import { Box, Flex, FlexProps, Heading, HStack, Image, Text, useBreakpointValue } from '@chakra-ui/react';
 import { ChakraRouterLink } from '../../../utils/ChakraRouterLink';
 import humanizeDuration from 'humanize-duration';
 import { reduceComponentsToString } from '../../../utils/StringUtils';
@@ -10,6 +10,8 @@ type SpotifyTrackProps = {
 }
 
 export function SpotifyTrack({ track, openInNewTab = false, ...rest }: SpotifyTrackProps & FlexProps) {
+  const shouldControlsBeNewLine = useBreakpointValue({ base: true, md: false });
+
   const artistsComponent = reduceComponentsToString(track.artists.map(artist => <ChakraRouterLink
     href={`/projects/spotify/artists/${artist.id}`} key={artist.id}
     target={openInNewTab ? '_blank' : '_self'}>{artist.name}</ChakraRouterLink>), ', ');
@@ -20,23 +22,28 @@ export function SpotifyTrack({ track, openInNewTab = false, ...rest }: SpotifyTr
     </ChakraRouterLink>;
   }
 
-  return <Flex {...rest} maxW={{ base: '100%', md: '95%' }}>
-    <TrackLink>
-      <Image boxSize={75} mr={2.5} src={track.album.images[0].url} alt='Spotify track preview image' />
-    </TrackLink>
-    <Box flex='1' my='auto'>
-      <Heading size='md'>
-        <TrackLink><b>{track.name}</b></TrackLink>
-      </Heading>
-      <HStack>
-        <Text fontSize='md'>By {artistsComponent}. Popularity: {track.popularity}%.
-          Duration: {humanizeDuration(track.duration_ms, { units: ['m', 's'], round: true })}</Text>
-        {track.preview_url && <Box>
-          <audio controls>
-            <source src={track.preview_url} type='audio/mp3' />
-          </audio>
-        </Box>}
-      </HStack>
-    </Box>
-  </Flex>;
+  const trackPreview = track.preview_url ? <Box>
+    <audio controls>
+      <source src={track.preview_url} type='audio/mp3' />
+    </audio>
+  </Box> : null;
+
+  return <Box {...rest}>
+    <Flex maxW={{ base: '100%', md: '95%' }}>
+      <TrackLink>
+        <Image boxSize={75} mr={2.5} src={track.album.images[0].url} alt='Spotify track preview image' />
+      </TrackLink>
+      <Box flex='1' my='auto'>
+        <Heading size='md'>
+          <TrackLink><b>{track.name}</b></TrackLink>
+        </Heading>
+        <Box as={shouldControlsBeNewLine ? Box : HStack}>
+          <Text fontSize='md'>By {artistsComponent}. Popularity: {track.popularity}%.
+            Duration: {humanizeDuration(track.duration_ms, { units: ['m', 's'], round: true })}</Text>
+          {!shouldControlsBeNewLine && trackPreview}
+        </Box>
+      </Box>
+    </Flex>
+    {shouldControlsBeNewLine && <Box my={2}>{trackPreview}</Box>}
+  </Box>;
 }
